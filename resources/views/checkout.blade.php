f5016684b39630c7407d36a702565fb754b4c9da
Long-Soeng167 <generalpurpose167@gmail.com>
2025-06-17 14:32:45 +0700

    update

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -9,7 +9,7 @@
 
   <link rel="stylesheet" href="https://assets.edlin.app/bootstrap/v5.3/bootstrap.css">
 
-  <script src="https://www.paypal.com/sdk/js?client-id={{config('paypal.client_id')}}&currency=USD&intent=capture"></script>
+  <script src="https://www.paypal.com/sdk/js?client-id={{config('paypal.client_id')}}&currency=GBP&intent=capture"></script>
 
   <!-- Title -->
   <title>PayPal Laravel</title>
@@ -19,44 +19,13 @@
   <div class="container text-center">
     <div class="row mt-3">
       <div class="col-12">
-        <img src="https://assets.edlin.app/logo/codewithross/logo-symbol-dark.png" height='100' alt="Ross Edlin Logo" />
-      </div>
-    </div>
-    <div class="row mt-3">
-      <div class="col-12">
         <h1>PayPal Laravel</h1>
-      </div>
-    </div>
-    <div class="row mt-2">
-      <div class="col-12">
-        <div class="links h5">
-          <a class="text-decoration-none mx-3" href="https://edlin.xyz/website" target="_blank">Home</a>
-          <a class="text-decoration-none mx-3" href="https://edlin.xyz/portfolio" target="_blank">Portfolio</a>
-          <a class="text-decoration-none mx-3" href="https://edlin.xyz/contact" target="_blank">Contact</a>
-          <a class="text-decoration-none mx-3" href="https://edlin.xyz/linkedin" target="_blank">LinkedIn</a>
-          <a class="text-decoration-none mx-3" href="https://edlin.xyz/github/paypal-laravel" target="_blank">GitHub</a>
-        </div>
       </div>
     </div>
     <div class="row mt-5">
       <div class="col-12">
         <div class="row">
           <div class="col-12 col-lg-6 offset-lg-3">
-            <p>Hey there, I'm a PayPal Payment Page.</p>
-
-            <p>Click the button below and you'll be taken to a <a href="https://developer.paypal.com/docs/checkout/"
-                target="_blank">PayPal</a>
-              checkout form where you can enter real credit / debit card details and send me money.</p>
-
-            <p>My purpose is to demonstrate building a <a href="https://laravel.com/docs/10.x/"
-                target="_blank">Laravel</a> / <a
-                href="https://developer.paypal.com/docs/checkout/" target="_blank">PayPal</a> app in 5 minutes.</p>
-
-            <p>You can see me building this app on <a href="https://edlin.xyz/youtube/paypal-laravel"
-                target="_blank">YouTube</a>
-              and view the <a href="https://edlin.xyz/github/paypal-laravel"
-                target="_blank">source code</a>.</p>
-
             <p class="text-danger">
               WARNING!!!<br />
               This is set to LIVE mode, so real money is used.<br />
@@ -76,14 +45,13 @@
         <div class="row mt-3">
           <div class="col-12 col-lg-6 offset-lg-3">
             <div class="input-group">
-              <span class="input-group-text">$</span>
+              <span class="input-group-text">£</span>
               <input type="text"
                 class="form-control"
                 id="paypal-amount"
-                value="0.5"
-                disabled
+                value="1"
                 aria-label="Amount (to the nearest pound)">
-              <!-- <span class="input-group-text">.00</span> -->
+              <span class="input-group-text">.00</span>
             </div>
           </div>
         </div>
@@ -91,12 +59,6 @@
         <div class="row mt-3">
           <div class="col-12 col-lg-6 offset-lg-3" id="payment_options"></div>
         </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-12 mt-3 mb-3">
-        Made with love, by <a href="https://www.rossedlin.com/" target="_blank">Ross Edlin</a> from <a
-          href="https://www.codewithross.com/" target="_blank">Code with Ross</a>.
       </div>
     </div>
   </div>
