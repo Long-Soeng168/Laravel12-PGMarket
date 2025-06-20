f3dec5ba7c348851bc8286991bd6ee701b86653d
Long-Soeng167 <generalpurpose167@gmail.com>
2025-06-20 13:59:10 +0700

    dd

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -1,11 +1,14 @@
 @extends('layout')
 @section('content')
-    <div class="row mt-5">
-        <div class="col-12">
-            <h1>Thanks for your order!</h1>
-            <p>
-                We appreciate your business!
-            </p>
-        </div>
+<div class="row mt-5">
+    <div class="col-12">
+        <h1>Thanks for your order!</h1>
+        <p>
+            We appreciate your business!
+        </p>
+        <a href="/">
+            <button>Home Page</button>
+        </a>
     </div>
+</div>
 @endsection
