e5f7895d4ef1fb81b3a334d7cb3a25f44d636d00
Long-Soeng167 <generalpurpose167@gmail.com>
2025-08-18 14:36:59 +0700

    update cancel showing status

--- resources/js/pages/nokor-tech/cart/ShoppingCart.tsx
+++ resources/js/pages/nokor-tech/cart/ShoppingCart.tsx
@@ -1,9 +1,11 @@
 import NokorTechLayout from '../layouts/nokor-tech-layout';
+import { CancelDialog } from './components/CancelDialog';
 import CartItemList from './components/CartItemList';
 
 export default function ShoppingCart() {
     return (
         <NokorTechLayout>
+            <CancelDialog />
             <CartItemList />
             <div className="h-20" />
         </NokorTechLayout>
