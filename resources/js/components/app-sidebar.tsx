5b1771965d06ba15dc3d0e6134db84de30983a60
Long-Soeng167 <generalpurpose167@gmail.com>
2025-08-19 17:03:07 +0700

    update

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -411,7 +411,7 @@ export function AppSidebar() {
         },
         {
             title: t('Shop Orders'),
-            permission: 'Shop',
+            // permission: 'Shop',
             url: '/shop-orders',
             icon: ClipboardListIcon,
         },
