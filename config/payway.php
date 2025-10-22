c551321a662ede3c67569f28c213d66cf8e9bc86
Long-Soeng167 <generalpurpose167@gmail.com>
2025-10-22 09:25:26 +0700

    update payway

--- config/payway.php
+++ config/payway.php
@@ -1,16 +1,16 @@
 <?php
 // config/payway.php
 return [
-    'api_url' => env('ABA_PAYWAY_API_URL', 'https://checkout-sandbox.payway.com.kh/api/payment-gateway/v1/payments/purchase'),
-    'base_api_domain' => env('ABA_BASE_API_DOMAIN', 'https://checkout-sandbox.payway.com.kh'),
+    'api_url' => env('ABA_PAYWAY_API_URL', 'https://checkout.payway.com.kh/api/payment-gateway/v1/payments/purchase'),
+    'base_api_domain' => env('ABA_BASE_API_DOMAIN', 'https://checkout.payway.com.kh'),
     // 'api_key' => env('ABA_PAYWAY_API_KEY', 'e72b3ee6918c544f42af460db2ffc9c77d9ca645'),
     // 'merchant_id' => env('ABA_PAYWAY_MERCHANT_ID', 'ec461385'),
-    'api_key' => env('ABA_PAYWAY_API_KEY', 'dfdb80b7c2adf6ad2f51e03e0ac29cee440d55e8'),
+    'api_key' => env('ABA_PAYWAY_API_KEY', '51ee1291-e350-4769-9e2a-1371bec8ef7f'),
     'merchant_id' => env('ABA_PAYWAY_MERCHANT_ID', 'ec437721'),
     'rsa_public_key' => env('ABA_RSA_PUBLIC_KEY', '-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDJ4DwJYd5hmTZI+CGbWG21hDPj
-P+fUIM0927HkpgjA6akaKPum0EdMpiL8lWmY/RU4Hlwd2lQVonJlUq85SsPV99qF
-34KozPeVquZgk2bgPWQyKO7vPyOm3el63TC8ugXjQHjkTMNV2a0idSNje8FzLDlL
-s+kNujfJdo2OQDYlrQIDAQAB
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC4Rb7V4kFlEi4SBGCtLDs7k/iu
+zGide8dFj01Vo3pSGDwAlgM9WXYdVHtttqirYXYgHrVOR3+LuMy0VVdh6+xGEmQn
+kfi8fQUd0h8c2OlwAqhMAZhZduSqHt1nHfJPgqPIeOXsNe6pwpVAS4ntRfSVhFYI
+k7YLVFXuMo11DOGgPwIDAQAB
 -----END PUBLIC KEY-----'),
 ];
