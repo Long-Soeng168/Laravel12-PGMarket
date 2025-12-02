fdccc6f9bccb672ce9fe38c4d8b228e3f1ee9953
Long-Soeng167 <generalpurpose167@gmail.com>
2025-12-02 15:06:36 +0700

    dd

--- resources/js/pages/nokor-tech/components/my-header.tsx
+++ resources/js/pages/nokor-tech/components/my-header.tsx
@@ -25,7 +25,7 @@ const MyHeader = () => {
         { label: t('Home'), href: '/' },
         { label: t('Products'), href: '/products' },
         { label: t('Shops'), href: '/shops' },
-        // { label: t('Privacy'), href: '/privacy' },
+        { label: t('Privacy'), href: '/privacy' },
         { label: t('About'), href: '/about-us' },
         { label: t('Contact'), href: '/contact-us' },
     ];
